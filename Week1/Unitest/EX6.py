4ebbc44a064958ebdd49bc4adc712aee93a0ed1d
Nattavee Narischat <nattavee123vee@gmail.com>
2022-12-20 23:35:31 +0700

    update ex6 but still bug

--- Week1/Unitest/EX6.py
+++ Week1/Unitest/EX6.py
@@ -6,59 +6,64 @@ class AdditionTestCase(unittest.TestCase):
                                             5,'2 2 2 2 2','2 2 2 2 2','2 2 2 2 2','2 2 2 2 2','2 2 2 2 2'])
     def test_sum(self, mock_inputs):
         test = sum_of_10_matrix()
-        result = test.calculate()
+        result = test.Input()
         assert result == [4,10]
         
-class sum_of_10_matrix:
+class sum_of_10_matrix():
     def __init__(self) :
-        self.Result=[]
-
-    def Input(self,column,rows,matrix):
-        self.value = []
+        self.matrix = []
+        self.Input = []
+    def Input(self):
         self.time = int(input("time : "))
-        self.value = input('value : ')
-        for i in range(self.time):
-            self.matrix = int(input("Matrix : "))
-           
-
-            for j in range(matrix):
-                value += value.split()
-
+        self.size = []
+        self.data_rows = []
+        self.data_columns =[]
+        time = int(input("time : "))
+        for i in range(time):
+            matrix = int(input("Matrix : "))
+            self.size.append(matrix)
+            value =[]
+            for i in range(self.size[i]):
+                value += input('value : ').split()
+            
             list_of_integers = [int(item) for item in value]
-            rows = []
-            rows_size = matrix
-
-            for i in range(0, len(list_of_integers), rows_size):
-                rows.append(list_of_integers[i:i+rows_size])
-            n = matrix 
+            rows =[]
+            for i in range(0, len(list_of_integers),self.size[-1]):
+                rows.append(list_of_integers[i:i+self.size[-1]])
+            
+            self.data_rows.append(rows)
+    
             columns = []
-
-            for i in range(n):
+            for i in range(self.size[-1]):
                 column = []
-                for j in range(n):
+                for j in range(self.size[-1]):
                     column.append(rows[j][i])
                 columns.append(column)
-
-    def calculate(self,rows,columns):
-        def find_count(matrix_value):
-                count = 0
-                for m in range (self.matrix):
-                    row = matrix_value[m]
+            self.data_columns.append(columns)
+        return self.data_rows,self.data_columns
+        
+    def find_count(matrix_value,size):
+            count = 0
+            for m in range (size):
+                row = matrix_value[m]
+                total = 0
+                for j in range(len(row)) :
+                    if total == 10:
+                            count +=1
                     total = 0
-                    for j in range(len(row)) :
+                    
+                    for i in range(j,len(row)):
                         if total == 10:
-                                count +=1
-                        total = 0
-                        
-                        for i in range(j,len(row)):
-                            if total == 10:
-                                count +=1
-                                total = 0
-                            elif total < 10:
-                                total+=row[i]
-                return count            
-        self.Result.append(find_count(self.rows)+find_count(self.columns))
-        print(self.Result)
+                            count +=1
+                            total = 0
+                        elif total < 10:
+                            total+=row[i]
+            return count
+                
+    def calculator(self):
+        for i in self.size:
+            Result += (self.find_count(self.data_rows[i],self.size[i])+self.find_count(self.data_columns[i],self.size[i]))
+        return Result
 
 if __name__ == "__main__":
     unittest.main()
